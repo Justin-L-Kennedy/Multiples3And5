ec088c91b5f19f7d7853c3fa5b35d2433a9ff944
Justin-L-Kennedy <justin.l.kennedy@outlook.com>
2020-11-02 21:57:18 -0600

    Multiples of 3

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -10,6 +10,18 @@ namespace Multiples3And5
     {
         static void Main(string[] args)
         {
+            //multiples of 3
+            int sumOfThrees = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                int multipleOfThree = i * 3;
+                if (multipleOfThree < 1000)
+                {
+                    Console.WriteLine(multipleOfThree); //show multiples of 3
+                    sumOfThrees += multipleOfThree;
+                }
+            }
+            Console.WriteLine(sumOfThrees); //show sum of the multiples of 3
         }
     }
 }
